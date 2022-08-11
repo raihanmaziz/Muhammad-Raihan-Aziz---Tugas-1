7041c86f73be6cc7580349724545ae7727b24aab
DESKTOP-B2GEA4J\A-22 <DESKTOP-B2GEA4J\A-22>
2022-08-11 15:08:58 +0800

    Adding boundry for Crazy Zombie

--- Assets/Scripts/ZombieCrazyController.cs
+++ Assets/Scripts/ZombieCrazyController.cs
@@ -6,6 +6,7 @@ public class ZombieCrazyController : BaseObject
 {
     private Rigidbody2D rig;
     private float timer;
+    private int clampBorderOffset = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +25,32 @@ public class ZombieCrazyController : BaseObject
             Move();
             timer = 0;
         }
+        ClampObjectIntoView(rig);
     }
 
     public override void Move()
     {
         _speed = new Vector2(Random.Range(-5f, 5f), -3);
         rig.velocity = _speed;
+    }
+
+    void ClampObjectIntoView(Rigidbody2D r)
+    {
+        Vector2 objectPosition = r.position;
+        float frustrumPositionRightX = Camera.main.ViewportToWorldPoint(new Vector2(1, 0)).x;
+        // Clamp right
+        if (objectPosition.x > frustrumPositionRightX - clampBorderOffset)
+        {
+            r.velocity = new Vector2(0, r.velocity.y);
+            r.position = new Vector2(frustrumPositionRightX - clampBorderOffset, r.position.y);
+        }
+        float frustrumPositionLeftX = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x;
+        // Clamp left
+        if (objectPosition.x < frustrumPositionLeftX + clampBorderOffset)
+        {
+            r.velocity = new Vector2(0, r.velocity.y);
+            r.position = new Vector2(frustrumPositionLeftX + clampBorderOffset, r.position.y);
+        }
+
     }
 }
